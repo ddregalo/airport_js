bba6fc7bea2220e5ae7542f55cc3563d68541a05
danielcampos <ddregalo@gmail.com>
2018-03-21 23:03:01 +0000

    Pass tests and implement airport clear to land function

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,10 +1,12 @@
 describe('Airport', function() {
-
   var airport;
+  var weather;
+  var plane;
 
   beforeEach(function(){
     airport = new Airport();
     plane = jasmine.createSpy('plane');
+    weather = new Weather();
   });
 
   it('creates an empty airport', function(){
@@ -18,9 +20,22 @@ describe('Airport', function() {
   });
 
   describe('_clearToLand', function(){
-    it('returns true to plane', function(){
-      expect(airport._clearToLand()).toEqual(true)
+    it('returns true to plane for landing', function(){
+      spyOn(Math, "random").and.returnValue(0.3);
+      // spyOn(weather, "isStormy").and.returnValue(false);
+      expect(airport._clearToLand(plane)).toEqual(true);
+    });
+
+    it('alert if weather too stormy for landing', function(){
+      spyOn(Math, "random").and.returnValue(0.9);
+      // spyOn(weather, "isStormy").and.returnValue(true);
+      spyOn(window, 'alert');
+      airport._clearToLand(plane);
+      expect(window.alert).toHaveBeenCalledWith('Weather is too stormy to land');
     });
   });
-
 });
+
+
+// var outerSpace = diamond.outerSpace.bind(diamond, 'D', 'C');
+//     expect(outerSpace).toThrow('Invalid combination of arguments');
--- src/airport.js
+++ src/airport.js
@@ -2,15 +2,27 @@ function Airport() {
   this.hangar = [];
 };
 
+
 Airport.prototype.landed = function() {
   return this.hangar;
 };
 
 Airport.prototype._clearToLand = function(plane) {
-  return true
+  var weather = new Weather();
+  if (weather.isStormy() == true) {
+    alert("Weather is too stormy to land");
+  } else {
+    return true
+  };
 };
 
 Airport.prototype.takeoff = function(plane) {
-  var index = this.hangar.indexOf(plane);
-  this.hangar.splice(index, 1)
+  var weather = new Weather();
+  if (weather.isStormy() === true) {
+    throw "Weather is too stormy to takeoff";
+  } else {
+    return true
+  };
+  // var index = this.hangar.indexOf(plane);
+  // this.hangar.splice(index, 1)
 };
