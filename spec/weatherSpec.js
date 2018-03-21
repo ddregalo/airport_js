55afed254290c2dbdd8df8f10b2c9ded671bfae4
danielcampos <ddregalo@gmail.com>
2018-03-21 17:46:49 +0000

    Finalize test for not stormy in weather function

--- spec/weatherSpec.js
+++ spec/weatherSpec.js
@@ -11,5 +11,12 @@ describe('Weather', function() {
       spyOn(Math, "random").and.returnValue(0.9);
       expect(weather.isStormy()).toEqual(true);
     });
+
+    it('returns false if not stormy', function(){
+      spyOn(Math, "random").and.returnValue(0.3);
+      expect(weather.isStormy()).toEqual(false);
+    });
+
   });
+
 });
