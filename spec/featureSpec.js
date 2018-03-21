594d5eff2f6126eb2869b407ea368c323af2029b
danielcampos <ddregalo@gmail.com>
2018-03-21 23:45:49 +0000

    Implement feature test to check planeAction is set to takeoff on clear from airport

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -11,11 +11,17 @@ describe("Dave's Airport", function() {
   });
 
   it("checks that plane action is equal to land on _clearToLand", function() {
-    spyOn(Math, "random").and.returnValue(false);
+    spyOn(Math, "random").and.returnValue(0.3);
     airport._clearToLand(plane);
     expect(plane.planeAction).toEqual('land');
   });
-  // 
+
+  it("checks that plane action is equal to takeoff on _clearToTakoff", function() {
+    spyOn(Math, "random").and.returnValue(0.3);
+    airport._clearToTakeoff(plane);
+    expect(plane.planeAction).toEqual('takeoff');
+  });
+  //
   // it("checks that the plane is no longer in the hanger after taking off", function(){
   //   airport.land('Dave');
   //   airport.takeoff('Dave');
